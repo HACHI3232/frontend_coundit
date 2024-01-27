3a7646790d2168eda5f3779778972117ac2ad0ef
HACHI3232 <harada.chisato526@gmail.com>
2024-01-28 01:30:49 +0900

    Homeページコンポーネントを追加

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import styles from '../styles/Home.module.css'
 import { CustomHead } from "../components/CustomHead"
 import { Header } from '../components/Header';
+import { Homepage } from '../components/Homepage';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <CustomHead />
       <Header/>
+      <Homepage />
     </div>
   )
 }
