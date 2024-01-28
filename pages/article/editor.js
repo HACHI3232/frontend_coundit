84d801afe9e5d3fb6a1526a35eb508d59128bdaa
HACHI3232 <harada.chisato526@gmail.com>
2024-01-28 21:38:17 +0900

    エラーをコメントアウト

--- pages/article/editor.js
+++ pages/article/editor.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import axios from "axios";
 import { CustomHead } from "../../components/CustomHead";
 
-
 export default function Editor() {
   const router = useRouter();
   const [title, setTitle] = useState("");
@@ -35,7 +34,7 @@ export default function Editor() {
         <div className="row">
           <div className="col-md-10 offset-md-1 col-xs-12">
             <ul className="error-messages">
-              <li>That title is required</li>
+              {/* <li>That title is required</li> */}
             </ul>
             <form onSubmit={handleSubmit}>
               <fieldset>
